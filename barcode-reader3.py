83bc3b747383f59c634e54ab807463120de6c95b
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-02-02 21:48:04 -0500

    Update barcode-reader3.py

--- barcode-reader3.py
+++ barcode-reader3.py
@@ -90,7 +90,7 @@ if img_file_buffer is not None:
                 
                 if 'brands' in response['product']:
                     df.loc["brands"]=response['product']['brands']
-                    if not df.loc["brands"].isempty()  and df.loc["brands"] in check_list:
+                    if not df.loc["brands"] and df.loc["brands"] in check_list:
                         st.write("Do Not Buy!!!")
                 else:
                   df.loc["barcode"]=""
